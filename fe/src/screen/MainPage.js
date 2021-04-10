dcf2c8357a02df8579de26da91eb8962452810cf
Zhu-Ruicong <traviszhuruicong@gmail.com>
2021-04-10 14:52:56 +0800

    add main page framework

---
+++ fe/src/screen/MainPage.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        //display: 'flex',
+    },
+    toolbar: {
+        //paddingRight: 24, // keep right padding when drawer closed
+    },
+
+    title: {
+        flexGrow: 1,
+    },
+
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        //flexGrow: 1,
+        //height: '100vh',
+        overflow: 'auto',
+    },
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+
+    },
+    paper: {
+        //padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+    calendarBox: {
+        height: 470,
+    },
+    outputBox: {
+        height: 380,
+    },
+    inputBox: {
+        height: 750,
+    },
+    runButton: {
+        height: 100,
+    },
+
+
+}));
+
+export default function MainPage() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="absolute" >
+                <Toolbar className={classes.toolbar}>
+                    <Typography component="h1" variant="h5" color="inherit" noWrap className={classes.title}>
+                        NUSchedule
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            <main className={classes.content}>
+                <div className={classes.appBarSpacer} />
+                <Container maxWidth="false" className={classes.container}>
+                    <Grid container spacing={4}>
+
+                        <Grid item xs={9}>
+                            <Grid container spacing = {3}>
+                                {/* Calendar */}
+                                <Grid item xs={12}>
+                                    <Paper className={classes.calendarBox}>
+                                        <Typography variant="h3">
+                                            {/* Replace with Calendar Component */}
+                                            Calendar
+                                        </Typography>
+                                    </Paper>
+                                </Grid>
+                                {/* Output */}
+                                <Grid item xs={12}>
+                                    <Paper className={classes.outputBox}>
+                                        <Typography variant="h3">
+                                            {/* Replace with Output Component */}
+                                            Output
+                                        </Typography>
+                                    </Paper>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+
+                        <Grid item xs={3}>
+                            <Grid container spacing = {3}>
+                                {/* Input */}
+                                <Grid item xs={12}>
+                                    <Paper className={classes.inputBox}>
+                                        <Typography variant="h3">
+                                            {/* Replace with Input Component */}
+                                            Input
+                                        </Typography>
+                                    </Paper>
+                                </Grid>
+                                {/* Button */}
+                                <Grid item xs={12}>
+                                    <Paper className={classes.runButton}>
+                                        <Typography variant="h3">
+                                            {/* Replace with Button Component */}
+                                            Button
+                                        </Typography>
+                                    </Paper>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+
+                    </Grid>
+                </Container>
+            </main>
+        </div>
+    );
+}
